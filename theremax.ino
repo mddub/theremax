de71dd0792132d09b272d05c8a4b5034aed993df
Mark Wilson <secretchinamark@gmail.com>
2013-04-07 17:28:15 -0500

    Notes are islands separated by areas where no sound is produced; wait for consecutive distances beyond min/max before calling notone().

--- theremax.ino
+++ theremax.ino
@@ -7,9 +7,9 @@ Based on http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/step
 #define speakerPin 9
 
 #include <NewPing.h>
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_SENSOR_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-NewPing sonar(trigPin, echoPin, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar(trigPin, echoPin, MAX_SENSOR_DISTANCE); // NewPing setup of pins and maximum distance.
 
 long currentDistance = 0;
 
@@ -17,8 +17,6 @@ unsigned long currentTime;
 double currentTone;
 
 double distanceCheckTimer[1] = {0};
-double toneUpdateTimer[1] = {0};
-double buzztimer[1] = {0};
 
 double cMajorScale[8] = {130.81, 146.83, 164.81, 174.61, 196.0, 220.0, 246.94, 261.63};
 
@@ -40,25 +38,48 @@ boolean tick(int delay, double timekeeper[1]){
   }
 }
 
+int consecutiveOutOfRange = 0;
+#define OFF_CYCLE_THRESHOLD 5
+
 void loop() {
   if(tick(50, distanceCheckTimer)) {
     updateDistance();
   }
-  currentTone = distanceToFreq(currentDistance);
-  if(currentTone == -1) {
-    noTone(speakerPin);
+  else {
+    return;
+  }
+  double newTone = distanceToFreq(currentDistance, currentTone);
+  if(newTone == -1) {
+    if(++consecutiveOutOfRange >= OFF_CYCLE_THRESHOLD) {
+      noTone(speakerPin);
+    }
   }
   else {
-    tone(speakerPin, currentTone); 
+    consecutiveOutOfRange = 0;
+    tone(speakerPin, newTone);
+    currentTone = newTone;
   }
 }
 
-double distanceToFreq(unsigned int distance) {
-  if(distance < 300 || distance > 2300) {
+const int MIN_DISTANCE = 300;
+const int NUM_NOTES = 8; // c major for now
+const double NOTE_WIDTH = 300.0;
+const double NOTE_BOUNDARY_WIDTH = 50;
+double MAX_DISTANCE = MIN_DISTANCE + NUM_NOTES * NOTE_WIDTH;
+
+double distanceToFreq(unsigned int distance, double currentTone) {
+  if(distance < MIN_DISTANCE || distance > MAX_DISTANCE) {
     return -1;
   }
-  int index = (distance - 300.0) / 250.0;
-  index = index % 8;
+
+  int index = (distance - MIN_DISTANCE) / NOTE_WIDTH;
+  index = index % NUM_NOTES;
+
+  // there is a valley between notes where no sound is produced
+  if(distance < MIN_DISTANCE + index * NOTE_WIDTH + NOTE_BOUNDARY_WIDTH || distance > MIN_DISTANCE + (index + 1) * NOTE_WIDTH - NOTE_BOUNDARY_WIDTH) {
+    return -1;
+  }
+
   return cMajorScale[index];
 }
 
@@ -66,7 +87,6 @@ void updateDistance() {
   unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
   if(uS) {
     currentDistance = uS;
-    //currentDistance = sonar.convert_cm(uS);
     Serial.print("Ping: ");
     Serial.println(currentDistance); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
   }
