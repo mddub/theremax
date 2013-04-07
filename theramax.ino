db69374286d06a2aac7802a81ff723e69aec1785
Mark Wilson <secretchinamark@gmail.com>
2013-04-07 03:09:01 -0500

    We have a C major scale! More importantly, we're using NewPing and tone() instead of managing time ourselves. Took another video.

--- theramax.ino
+++ theramax.ino
@@ -2,68 +2,72 @@
 Based on http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/step3/Upload-the-sketch/
 */
 
-#include <Time.h>
-
 #define trigPin 13
 #define echoPin 12
 #define speakerPin 9
-#define led 11
-#define led2 10
 
-long elapsed;
+#include <NewPing.h>
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(trigPin, echoPin, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
 long currentDistance = 0;
 
-int state;
-const int STATE_WAITING_FOR_ECHO = 0;
-const int STATE_RECEIVED_DISTANCE = 1;
-long ms_since_echo = 0;
+unsigned long currentTime;
+double currentTone;
+
+double distanceCheckTimer[1] = {0};
+double toneUpdateTimer[1] = {0};
+double buzztimer[1] = {0};
+
+double cMajorScale[8] = {130.81, 146.83, 164.81, 174.61, 196.0, 220.0, 246.94, 261.63};
 
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
   pinMode (speakerPin, OUTPUT);
-  elapsed = 0;
+}
+
+boolean tick(int delay, double timekeeper[1]){
+  currentTime = millis();
+  if(currentTime >= (timekeeper[0] + delay)){
+    timekeeper[0] = currentTime;
+    return true;
+  }
+  else {
+    return 0;
+  }
 }
 
 void loop() {
-  doDistanceStuff(elapsed);
-  buzz(currentDistance * 4000.0 / 24.0);
-  
-  elapsed++;
-  delayMicroseconds(1);
-}
-
-void doDistanceStuff(long elapsed) {
-  if(elapsed % 12 == 0) {
-    digitalWrite(trigPin, LOW);
-    long duration = pulseIn(echoPin, HIGH);
-    currentDistance = (duration/2) / 29.1;
-    Serial.print(currentDistance);
-    Serial.println(" cm");
+  if(tick(50, distanceCheckTimer)) {
+    updateDistance();
   }
-  else if(elapsed % 12 == 2) {
-    digitalWrite(trigPin, HIGH);
+  currentTone = distanceToFreq(currentDistance);
+  if(currentTone == -1) {
+    noTone(speakerPin);
+  }
+  else {
+    tone(speakerPin, currentTone); 
   }
 }
 
-long getDistance() {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-
-  return distance;
+double distanceToFreq(unsigned int distance) {
+  if(distance < 300 || distance > 2300) {
+    return -1;
+  }
+  int index = (distance - 300.0) / 250.0;
+  index = index % 8;
+  return cMajorScale[index];
 }
 
-void buzz(int tone) {
-    digitalWrite(speakerPin, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(speakerPin, LOW);
-    delayMicroseconds(tone);
+void updateDistance() {
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  if(uS) {
+    currentDistance = uS;
+    //currentDistance = sonar.convert_cm(uS);
+    Serial.print("Ping: ");
+    Serial.println(currentDistance); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  }
 }
